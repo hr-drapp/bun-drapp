6f1c1f29f4a4c7acc069a33b1e0a52e69d758f78
harsh-tsx <probookh118@gmail.com>
2026-05-22 11:14:25 +0530

    bug fixes

--- deploy.prod.sh
+++ deploy.prod.sh
@@ -3,6 +3,7 @@
 echo "Starting deployment..."
 sudo -u root whoami
 sudo -u root su
+sudo -u bun install
 # Reload the application using pm2
 sudo -u root service app1 restart
 echo "Deployment completed successfully."
